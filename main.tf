7ab9e329f38253fd25d194b0f62a6ad43bb3fe7e
ronaldnnr <ronaldnnr@gmail.com>
2023-05-07 03:00:24 -0300

    first commit 3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ec2_bgr_infra" {
-  source   = "./ec2_module"
-  key_name = "kubernetes-cka"
-  ami           = "ami-0a695f0d95cefc163"
-  instance_type = "t2.medium"
+  source         = "./ec2_module"
+  key_name       = "kubernetes-cka"
+  ami            = "ami-0a695f0d95cefc163"
+  instance_type  = "t2.medium"
 }
